0633f9dad24f6a301d128df7d0ca49d20fe43f79
Gergely Mentsik <gergely@mentsik.eu>
2017-02-07 23:02:34 +0100

    Fixed warnings in package zip.

--- src/main/java/ly/gerge/rancher2git/zip/ZipAgent.java
+++ src/main/java/ly/gerge/rancher2git/zip/ZipAgent.java
@@ -3,9 +3,8 @@ package ly.gerge.rancher2git.zip;
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 
-/**
- * Created by gerge on 05.02.2017.
- */
+/*** Created by Gergely Mentsik on 05.02.2017 */
+
 public class ZipAgent {
 
     public static void unzip(String source, String destination) throws ZipException {
