1287be8c3c10da85fde31a9f2bd4685a048816ea
Gergely Mentsik <gergely@mentsik.eu>
2017-02-17 23:04:24 +0100

    Added test for cloning a repo.

--- src/test/java/ly/gerge/rancher2git/Rancher2gitTest.java
+++ src/test/java/ly/gerge/rancher2git/Rancher2gitTest.java
@@ -3,6 +3,7 @@ package ly.gerge.rancher2git;
 import ly.gerge.rancher2git.repo.GitRepo;
 import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,7 +31,14 @@ class Rancher2gitTest {
     //Clone https://github.com/twbs/bootstrap.git
     @Test
     void test_CloneRepo() throws Exception {
+        String testRepo = "https://github.com/twbs/bootstrap.git";
+        String tmpDir = "test-repo";
 
+        GitRepo gitRepo = new GitRepo(tmpDir,testRepo);
+        gitRepo.close();
+        assertTrue(Files.isDirectory(new File(tmpDir+"/.git").toPath()));
+
+        FileUtils.cleanDirectory(new File(tmpDir));
     }
 
 }
