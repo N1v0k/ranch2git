6b767b23b75cc8ff7f614006089e48bfd1787df5
Gergely Mentsik <gergely@mentsik.eu>
2017-02-17 23:16:32 +0100

    Changed repo cloning test

--- src/test/java/ly/gerge/rancher2git/Rancher2gitTest.java
+++ src/test/java/ly/gerge/rancher2git/Rancher2gitTest.java
@@ -1,44 +1,31 @@
 package ly.gerge.rancher2git;
 
 import ly.gerge.rancher2git.repo.GitRepo;
-import org.apache.commons.io.FileUtils;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.junit.Assert;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.nio.file.Files;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /*** Created by Gergely Mentsik on 17.02.2017. */
 
-class Rancher2gitTest {
-    @BeforeEach
-    void setUp() {
+public class Rancher2gitTest {
+    private String testDir= "tests";
 
-    }
-
-    @AfterEach
-    void tearDown() {
-
+    public Rancher2gitTest() {
     }
 
     //Clone https://github.com/twbs/bootstrap.git
     @Test
-    void test_CloneRepo() throws Exception {
+    public void test_CloneRepo() throws Exception {
         String testRepo = "https://github.com/twbs/bootstrap.git";
-        String tmpDir = "test-repo";
+        String tmpDir = testDir + "/test-repo";
 
         GitRepo gitRepo = new GitRepo(tmpDir,testRepo);
         gitRepo.close();
         assertTrue(Files.isDirectory(new File(tmpDir+"/.git").toPath()));
-
-        FileUtils.cleanDirectory(new File(tmpDir));
     }
 
 }
