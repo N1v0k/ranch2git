43a2599250049bf4d3ec37167cb517299d3fdbb8
Gergely Mentsik <gergely@mentsik.eu>
2017-02-23 00:26:37 +0100

    Added support for v1 API

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,31 @@
 #!/usr/bin/env bash
 
-while true; do
+if [ -n "$RANCHER_APIV2_URL" ]; then
+    while true; do
 
- java -jar /root/rancher2git.jar \
-   ${RANCHER_APIV2_URL} ${RANCHER_API_USER} ${RANCHER_API_PASS}  \
-   ${GIT_REPO_URL} ${GIT_REPO_USER} ${GIT_REPO_PASS};  \
+     java -jar /root/rancher2git.jar \
+       ${RANCHER_APIV2_URL} ${RANCHER_API_USER} ${RANCHER_API_PASS}  \
+       ${GIT_REPO_URL} ${GIT_REPO_USER} ${GIT_REPO_PASS};  \
 
- sleep ${UPDATE_EVERY};
+     sleep ${UPDATE_EVERY};
 
-done
+    done
+fi
+
+if [ -n "$CATTLE_URL" ]; then
+    while true; do
+
+     java -jar /root/rancher2git.jar \
+       ${CATTLE_URL} ${CATTLE_ACCESS_KEY} ${CATTLE_SECRET_KEY}  \
+       ${GIT_REPO_URL} ${GIT_REPO_USER} ${GIT_REPO_PASS};  \
+
+     sleep ${UPDATE_EVERY};
+
+    done
+fi
+
+echo "You have to provide your Rancher API URL either by setting RANCHER_APIV2_URL environment variable or the tags: "
+echo "io.rancher.container.create_agent=true"
+echo "io.rancher.container.agent.role=environment"
+
+exit -1
